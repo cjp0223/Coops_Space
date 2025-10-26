a8ecaba602136790da913d1c81a9edf81dcb56fc
Cooper Phillips <cjohnphillips@icloud.com>
2025-10-26 15:48:50 -0600

    Here's my submission

---
+++ setup.R
@@ -0,0 +1,25 @@
+packageLoad<-
+  function(x) {
+    for (i in 1:length(x)) {
+      if(!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i],character.only = TRUE)
+    }
+  }
+
+packages<- c('tidyverse',
+             'palmerpenhuins',
+             'sf',
+             'terra',
+             'tmap',
+             'rmarkdown',
+             'tigris',
+             'elevatr',
+             'rgdal')
+
+packageLoad(packages)
+
+
+
+
